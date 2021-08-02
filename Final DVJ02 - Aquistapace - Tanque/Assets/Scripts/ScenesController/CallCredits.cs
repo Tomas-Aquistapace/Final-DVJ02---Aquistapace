91c10d8cf556453d4207bb78b2eb0832f3212a6a
Tomas-Aquistapace <Aquista35@gmail.com>
2021-08-02 20:42:07 -0300

    Stylized Menu

---
+++ Final DVJ02 - Aquistapace - Tanque/Assets/Scripts/ScenesController/CallCredits.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CallCredits : MonoBehaviour
+{
+    [SerializeField] private Animator credits;
+    [SerializeField] private GameObject closeButton;
+
+    public void CallCredit()
+    {
+        credits.SetTrigger("Activate");
+
+        //closeButton.SetActive(true);
+    }
+
+    public void CloseCredit()
+    {
+        credits.SetTrigger("Activate");
+
+        //closeButton.SetActive(false);
+    }
+
+}
